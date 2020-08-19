cfa453f59c1c0ea4620f35bd670c9231e7710785
helloamandamurphy <helloamandamurphy@gmail.com>
2020-08-19 15:22:29 -0700

    Add quoteData and render randomized quote in QuoteScreen

--- app/screens/QuoteScreen.js
+++ app/screens/QuoteScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   StyleSheet,
   Button,
@@ -8,23 +8,44 @@ import {
   TouchableOpacity,
 } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
+import quoteData from "../quoteData";
+
+// const quote = quoteData[Math.floor(Math.random() * quoteData.length)];
+// const author = quote.author;
 
 function QuoteScreen({ navigation }) {
-    return (
-        <Button
-            title="Take Pic"
-            style={styles.someButton}
-            onPress={() => navigation.navigate("Camera")} //must match any name prop given in App.js for the Stack Screen
-        ></Button>
-);}
-    
+  return (
+    <React.Fragment>
+      <Text style={styles.quoteText}>"{quoteText}"</Text>
+      <Text style={styles.authorName}>-{authorName}</Text>
+      <Button
+        title="Take Pic"
+        style={styles.someButton}
+        onPress={() => navigation.navigate("Camera")} //must match any name prop given in App.js for the Stack Screen
+      ></Button>
+    </React.Fragment>
+  );
+}
+
 const styles = StyleSheet.create({
-    someButton: {
-        color: "red",
-        justifyContent: "flex-start"
-    }
+  someButton: {
+    color: "red",
+    justifyContent: "flex-start",
+  },
+
+  quoteText: {
+    fontSize: 24,
+    padding: 20,
+  },
+
+  authorName: {
+    // justifyContent: "right",
+    padding: 20,
+  },
 });
-        
-    
+
+const quoteInfo = quoteData[Math.floor(Math.random() * quoteData.length)];
+const quoteText = quoteInfo.quote;
+const authorName = quoteInfo.author;
 
 export default QuoteScreen;
