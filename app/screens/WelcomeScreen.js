74d799cfdfe397f91f2d9e6d6084aeaef2700d34
helloamandamurphy <helloamandamurphy@gmail.com>
2020-08-20 12:49:03 -0700

    Some tweaks to WelcomeScreen to get back the missing button; add background image and some formatting to QuoteScreen; add background image to assets

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -5,18 +5,20 @@ import {
   ImageBackground,
   Text,
   View,
-  TouchableOpacity, Dimensions
+  TouchableOpacity,
+  Dimensions,
 } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
-import { Video } from 'expo-av';
+import { Video } from "expo-av";
 
 const { width } = Dimensions.get("window");
 
 function WelcomeScreen({ navigation }) {
-    return (
+  return (
+    <React.Fragment>
       <View>
         <Video
-          source={require("../assets/GoBananas!.mp4")}
+          source={require("../assets/GoBananas.mp4")}
           rate={1.0}
           volume={1.0}
           isMuted={false}
@@ -24,25 +26,27 @@ function WelcomeScreen({ navigation }) {
           shouldPlay
           isLooping
           style={{ width: "100%", height: "100%" }}
-        >
-          <Button
-            title="Get Quote"
-            style={styles.someButton}
-            onPress={() => navigation.navigate("Quote")} //must match any name prop given in App.js for the Stack Screen
-          ></Button>
-        </Video>
+        ></Video>
       </View>
-    );
+      <View style={styles.someButton}>
+        <Button
+          title="Get Quote"
+          // style={styles.someButton}
+          onPress={() => navigation.navigate("Quote")} //must match any name prop given in App.js for the Stack Screen
+        ></Button>
+      </View>
+    </React.Fragment>
+  );
 }
 
-const styles = StyleSheet.create({ //button wont move down...
+const styles = StyleSheet.create({
+  //button wont move down...
   someButton: {
     justifyContent: "flex-end",
-        position: "absolute",
-        top: 300,
-        alignContent: 'flex-end',
-        flex: 1
-    
+    position: "absolute",
+    bottom: 0,
+    alignContent: "flex-end",
+    flex: 1,
   },
 });
 
