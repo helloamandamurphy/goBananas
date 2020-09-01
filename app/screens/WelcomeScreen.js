625692dd733b6d726b9030424a5401e44aed72c6
Laura Evans <lauraevans@Lauras-MacBook-Air.local>
2020-09-01 18:34:52 -0400

    added final styling to welcomeScreen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -6,7 +6,7 @@ import {
   Text,
   View,
   TouchableOpacity,
-  Dimensions, Linking, SafeAreaView
+  Dimensions, Linking, SafeAreaView, Image
 } from "react-native";
 import { TextInput, TouchableHighlight } from "react-native-gesture-handler";
 import { Video } from "expo-av";
@@ -15,92 +15,62 @@ const { width } = Dimensions.get("window");
 
 function WelcomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text>
-          Inpsired By the
-          <TouchableOpacity
-            onPress={() =>
-              Linking.openURL(
-                "https://www.aboutamazon.com/the-community-banana-stand"
-              )
-            }
-          >
-            <Text style={styles.quoteButton}> Amazon Banana Stands!</Text>
-          </TouchableOpacity>
-        </Text>
-      </View>
+    <View>
       <Video
         source={require("../assets/GoBananas2.mp4")}
         rate={1.0}
         volume={1.0}
         isMuted={false}
-        resizeMode="contain"
+        resizeMode="cover"
         shouldPlay
         isLooping
-        style={{ width: "100%", height: "100%" }}
+        style={styles.video}
       >
-        <TouchableOpacity onPress={() => navigation.navigate("Game")}>
-          <Text style={styles.quoteButton}>
-            Play to get a Banana Quote of the day!
-          </Text>
-        </TouchableOpacity>
+        <View style={styles.buttonSpace}>
+          <TouchableOpacity onPress={() => navigation.navigate("Game")}>
+            <Image
+              style={styles.playButton}
+              source={require("../assets/cute.gif")}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.articleLink}>
+          <Button
+            title="Inspired by the Amazon Banana Stands!"
+            onPress={() =>
+              Linking.openURL(
+                "https://www.aboutamazon.com/the-community-banana-stand"
+              )
+            }
+          ></Button>
+        </View>
       </Video>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  quoteButton: {
-    backgroundColor: 'pink',
-    //flex: 1,
-    //justifyContent: 'flex-end'
+  buttonSpace: {
+    top: 400,
+    left: 100,
+    zIndex: 2
   },
-  linkBox: {
-    flex: 1
+  playButton: {
+    width: 200,
+    height: 150,
+    borderRadius: 45,
   },
-  container: {
-    flex: 1,
+  video: {
+    width: "100%",
+    height: "100%",
+    zIndex: 0,
   },
-  someButton: {
-    justifyContent: "center",
+  articleLink: {
+    zIndex: 2,
     position: "absolute",
-    bottom: 40,
-    left: "38%",
-    alignContent: "flex-end",
-    flex: 1,
-    backgroundColor: "teal",
-    //color: "white"
-  },
-  bottomView: {
-    backgroundColor: "pink",
-    height: "50%",
-    flex: 1,
-    alignContent: "center",
-    justifyContent: "center",
-  },
-  textLink: {
-    // alignContent: "center",
-    // justifyContent: 'center',
-    // position: "absolute",
-    // top: 255,
-    // left: 48,
-    fontFamily: "Arial",
-    fontSize: 20,
-    fontStyle: "italic",
+    top: 700,
+    left: 50,
     color: "pink",
-  },
-  textBox: {
-    backgroundColor: "teal",
-    height: "10%",
-    width: "86%",
-    position: "absolute",
-    top: "30%",
-    left: "5%",
-    borderRadius: 10,
-    alignContent: "center",
-    justifyContent: "center",
-     flex: 1,
   },
 });
 
